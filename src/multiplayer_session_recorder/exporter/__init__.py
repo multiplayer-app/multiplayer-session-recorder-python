9a5d4acf5145acec45f5212b0c5e35702960e2c1
Dmytro Shvaika <shvajka@gmail.com>
2025-08-25 00:46:48 +0300

    fix imports

--- src/multiplayer_session_recorder/exporter/__init__.py
+++ src/multiplayer_session_recorder/exporter/__init__.py
@@ -2,14 +2,14 @@ from .http.trace_exporter import OTLPSpanExporter as HTTPOTLPSpanExporter
 from .http.log_exporter import OTLPLogExporter as HTTPOTLPLogExporter
 from .grpc.trace_exporter import OTLPSpanExporter as GRPCOTLPSpanExporter
 from .grpc.log_exporter import OTLPLogExporter as GRPCOTLPLogExporter
-from .span_wrapper import MultiplayerOTLPSpanExporterWrapper
-from .log_wrapper import MultiplayerOTLPLogExporterWrapper
+from .span_wrapper import OTLPSpanExporterWrapper
+from .log_wrapper import OTLPLogExporterWrapper
 
 __all__ = [
     "HTTPOTLPSpanExporter",
     "HTTPOTLPLogExporter", 
     "GRPCOTLPSpanExporter",
     "GRPCOTLPLogExporter",
-    "MultiplayerOTLPSpanExporterWrapper",
-    "MultiplayerOTLPLogExporterWrapper"
+    "OTLPSpanExporterWrapper",
+    "OTLPLogExporterWrapper"
 ]
